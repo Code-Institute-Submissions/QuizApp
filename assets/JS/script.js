44497f6b0422b15a5fd267f442d22d20e514e756
arpreacher <anishrpillai@hotmail.com>
2022-06-09 09:16:03 +0000

    First commit html, css and JS

---
+++ assets/JS/script.js
@@ -0,0 +1,9 @@
+let quizObjects = [
+    {
+        question : "Most popular programming languuage ?",
+        first: "Ruby",
+        second: "Python",
+        third: "JavaScript",
+        answer : "JavaScript"
+    },
+]

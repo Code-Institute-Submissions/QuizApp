c2e8455134cd2bcac03e6d5d123c83f96ea2f9df
arpreacher <anishrpillai@hotmail.com>
2022-06-09 09:55:15 +0000

    DOM values inserted to the landing page

--- assets/JS/script.js
+++ assets/JS/script.js
@@ -10,17 +10,22 @@ let quizObjects = [
     },
 ]
 
-console.log(quizObjects[0].question);
+console.log(quizObjects[0].first);
 // Pull elements from DOM
 
 let mainHead = document.querySelector('.main-head');
-
+let firstLabel = document.querySelector('.firstLabel');
+let secondLabel = document.querySelector('.secondLabel');
+let thirdLabel = document.querySelector('.thirdLabel');
 /**
  * First task
  * Display user interface
  */
 function displayQuestionChoices() {
     mainHead.innerHTML = quizObjects[0].question;
+    firstLabel.innerHTML = quizObjects[0].first;
+    secondLabel.innerHTML = quizObjects[0].second;
+    thirdLabel.innerHTML = quizObjects[0].third;
     //alert("Page testing");
 }
 

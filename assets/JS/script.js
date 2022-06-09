50b6b1b9b96afb29fb644166c4dc04d4dfb476e3
arpreacher <anishrpillai@hotmail.com>
2022-06-09 11:19:49 +0000

    Add function to check the responses logged

--- assets/JS/script.js
+++ assets/JS/script.js
@@ -1,22 +1,21 @@
+let quizObjects = [{
+    question: "The most popular programming language?",
+    first: "Ruby",
+    second: "Python",
+    third: "JavaScript",
+    answer: "third"
+}, ]
 
-
-let quizObjects = [
-    {
-        question: "The most popular programming language?",
-        first: "Ruby",
-        second: "Python",
-        third: "JavaScript",
-        answer: "JavaScript"
-    },
-]
-
-console.log(quizObjects[0].first);
+//console.log(quizObjects[0].first);
 // Pull elements from DOM
 
 let mainHead = document.querySelector('.main-head');
 let firstLabel = document.querySelector('.firstLabel');
 let secondLabel = document.querySelector('.secondLabel');
 let thirdLabel = document.querySelector('.thirdLabel');
+let submitButton = document.getElementById('submit');
+let response = document.querySelectorAll('.choices');
+//console.log('responses are', response);
 /**
  * First task
  * Display user interface
@@ -29,6 +28,30 @@ function displayQuestionChoices() {
     //alert("Page testing");
 }
 
+/**
+ * Second task
+ * to find out if the answer submitted is correct or not
+ */
+function submitResponse() {
+    submitButton.addEventListener('click', function () {
+        getUserResponse()
+    })
+
+}
+
+function getUserResponse() {
+    let userResponse
+    response.forEach(function (choices) {
+        //console.log(choices);
+        if (choices.checked === true) {
+            userResponse = choices.id
+
+        }
+
+    })
+    console.log(userResponse);
+}
 
 // Calling Function
 displayQuestionChoices()
+submitResponse()
